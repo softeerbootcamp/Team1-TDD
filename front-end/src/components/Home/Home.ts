c44a15d436d9c759c7d4ef63972a571c105c1ee7
MyuB <mjycoco0@naver.com>
2023-02-05 00:56:25 +0900

    feat: scoll시 opacity 변경 애니메이션 추가

--- front-end/src/components/Home/Home.ts
+++ front-end/src/components/Home/Home.ts
@@ -1,8 +1,28 @@
 import Component from "@/core/Component";
+import { qsa } from "@/utils/querySelector";
+import styles from "./Home.module.scss";
 
 import { homeTemplate } from "./template";
 export class Home extends Component {
   template(): string {
     return homeTemplate();
+  }
+
+  mounted(): void {
+    const observer: IntersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const target = entry.target as HTMLDivElement;
+            target.style.opacity = "1";
+            observer.unobserve(target);
+          }
+        });
+      }
+    );
+    const indexes = qsa(`.${styles.idxes}`);
+    indexes.forEach((indexWrapper) => {
+      observer.observe(indexWrapper);
+    });
   }
 }
