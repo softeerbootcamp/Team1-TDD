131a1b805927d65021625ca9fa04ae0ea7277774
River-Mt <rivermt1998@gmail.com>
2023-02-06 14:36:14 +0900

    refactor: PostCotroller를 PostController로 변경하여 오타 수정

--- back-end/src/main/java/com/tdd/backend/post/PostController.java
+++ back-end/src/main/java/com/tdd/backend/post/PostController.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-public class PostCotroller {
+public class PostController {
 	private final PostService postService;
 
 	@PostMapping("/sharing")
@@ -55,6 +55,7 @@ public class PostCotroller {
 	@Operation(summary = "특정 차종에 대한 모든 옵션 리스트 렌더링", description = "시승, 공유 모두 사용하며, 차종 요청에 대해 가능한 모든 옵션을 제공해야 함.")
 	public void getOptions(@PathVariable String name) {
 		// 응답: {name}에 존재하는 모든 옵션 리스트 JSON
+
 	}
 
 }
