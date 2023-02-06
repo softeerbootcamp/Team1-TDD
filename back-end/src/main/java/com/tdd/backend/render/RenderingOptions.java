418de7e5b02a1a822c6b79bc02d68846cc9e7bc1
River-Mt <rivermt1998@gmail.com>
2023-02-07 00:50:27 +0900

    feat: RenderingOption에 name 추가

--- back-end/src/main/java/com/tdd/backend/render/RenderingOptions.java
+++ back-end/src/main/java/com/tdd/backend/render/RenderingOptions.java
@@ -4,23 +4,24 @@ import static com.tdd.backend.post.Category.*;
 
 import com.tdd.backend.post.Category;
 
+import lombok.Getter;
+
+@Getter
 public enum RenderingOptions {
-	LAMP(SAFETY), //Safety option
-	SUNROOF(SAFETY), //Safety option
-	MONITOR(DISPLAY),  //Display option
-	RED(COLOR), //Color
-	BLUE(COLOR), //color
-	DIESEL(ENGINE), //engine
-	LPG(ENGINE); //engine
+	LAMP(SAFETY, "LED 헤드램프"), //Safety option
+	SUNROOF(SAFETY, "썬루프"), //Safety option
+	MONITOR(DISPLAY, "모니터"),  //Display option
+	RED(COLOR, "RED"), //Color
+	BLUE(COLOR, "BLUE"), //color
+	DIESEL(ENGINE, "디젤"), //engine
+	LPG(ENGINE, "LPG"); //engine
 
 	private final Category category;
+	private final String name;
 
-	RenderingOptions(Category category) {
+	RenderingOptions(Category category, String name) {
 		this.category = category;
-	}
-
-	public String getCategory() {
-		return category.toString();
+		this.name = name;
 	}
 
 }
