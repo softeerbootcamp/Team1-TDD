be415f53a216f2db67365a91f096a2a4c5ec7180
River-Mt <rivermt1998@gmail.com>
2023-02-06 15:49:30 +0900

    refactor: 특정 차에 관한 옵션을 SpecificCarOptions에서 다루기 때문에 쓸모 없어진 메서드 삭제

--- back-end/src/main/java/com/tdd/backend/post/RenderingOptions.java
+++ back-end/src/main/java/com/tdd/backend/post/RenderingOptions.java
@@ -2,9 +2,6 @@ package com.tdd.backend.post;
 
 import static com.tdd.backend.post.Category.*;
 
-import java.util.Collections;
-import java.util.List;
-
 public enum RenderingOptions {
 	LAMP(SAFETY), //Safety option
 	SUNROOF(SAFETY), //Safety option
@@ -15,14 +12,6 @@ public enum RenderingOptions {
 	LPG(ENGINE); //engine
 
 	RenderingOptions(Category category) {
-	}
-
-	public List<RenderingOptions> getCommonOptions() {
-		return List.of(LAMP, SUNROOF, MONITOR);
-	}
-
-	public List getAvanteOptions() {
-		return Collections.singletonList(List.of(RED, COLOR).addAll(getCommonOptions()));
 	}
 
 }
