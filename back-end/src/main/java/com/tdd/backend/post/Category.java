e1dc1c83791216e7ddc40c2a071f1b8d106d4b97
River-Mt <rivermt1998@gmail.com>
2023-02-07 00:44:57 +0900

    feat: Category 각 항목에 name 추가

--- back-end/src/main/java/com/tdd/backend/post/Category.java
+++ back-end/src/main/java/com/tdd/backend/post/Category.java
@@ -1,8 +1,18 @@
 package com.tdd.backend.post;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-	SAFETY,
-	DISPLAY,
-	COLOR,
-	ENGINE
+	SAFETY("안전/성능"),
+	DISPLAY("내장/외장"),
+	COLOR("색상"),
+	ENGINE("엔진");
+
+	private final String name;
+
+	Category(String name) {
+		this.name = name;
+	}
+
 }
