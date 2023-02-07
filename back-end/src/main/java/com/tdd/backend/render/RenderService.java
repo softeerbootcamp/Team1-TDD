88e6a2d405b836bb2369028354382387bef87891
River-Mt <rivermt1998@gmail.com>
2023-02-07 20:58:01 +0900

    refactor: 컨벤션에 맞게끔 코드 수정, OptionDto 이름 변경사항으로 인한 변경

--- back-end/src/main/java/com/tdd/backend/render/RenderService.java
+++ back-end/src/main/java/com/tdd/backend/render/RenderService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.tdd.backend.post.Category;
-import com.tdd.backend.render.data.Option;
+import com.tdd.backend.render.data.OptionDto;
 import com.tdd.backend.render.data.OptionResponse;
 
 @Service
@@ -16,24 +16,25 @@ public class RenderService {
 		List<OptionResponse> response = new ArrayList<>();
 
 		for (Category category : Category.values()) {
-			List<Option> optionList = new ArrayList<>();
+			List<OptionDto> optionList = new ArrayList<>();
 
 			for (RenderingOption renderOption : list) {
 				if (isCategoryEquals(category, renderOption.getCategory())) {
-					Option option = new Option(renderOption.getName(), renderOption.getCategory().getName());
+					OptionDto option = new OptionDto(renderOption.getName(), renderOption.getCategory().getName());
 					optionList.add(option);
 				}
 			}
-			if (!optionList.isEmpty())
+			if (!optionList.isEmpty()) {
 				response.add(OptionResponse.builder().category(category.getName()).options(optionList).build());
+			}
 
 		}
 
 		return response;
 	}
 
-	private boolean isCategoryEquals(Category A, Category B) {
-		return A.equals(B);
+	private boolean isCategoryEquals(Category categoryA, Category categoryB) {
+		return categoryA.equals(categoryB);
 	}
 
 }
