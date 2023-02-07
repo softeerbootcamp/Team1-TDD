31d70c9792b8d1ccfc833cbb898a610d343d10dd
River-Mt <rivermt1998@gmail.com>
2023-02-07 17:12:15 +0900

    feat: 옵션들 추가

--- back-end/src/main/java/com/tdd/backend/render/RenderingOption.java
+++ back-end/src/main/java/com/tdd/backend/render/RenderingOption.java
@@ -8,13 +8,30 @@ import lombok.Getter;
 
 @Getter
 public enum RenderingOption {
-	LAMP(SAFETY, "LED 헤드램프"), //Safety option
-	SUNROOF(SAFETY, "썬루프"), //Safety option
-	MONITOR(DISPLAY, "모니터"),  //Display option
-	RED(COLOR, "RED"), //Color
-	BLUE(COLOR, "BLUE"), //color
-	DIESEL(ENGINE, "디젤"), //engine
-	LPG(ENGINE, "LPG"); //engine
+
+	PARKING_DISTANCE_WARNING_FRONT(SAFETY_OR_PERFORMANCE, "주차거리경고-전방"),
+	PARKING_DISTANCE_WARNING_BACK(SAFETY_OR_PERFORMANCE, "주차거리경고-후방"),
+	VIBRATION_WARNING_STEERING_WHEEL(SAFETY_OR_PERFORMANCE, "진동경고 스티어링 휠"),
+
+	BCA(INTELLIGENT_TECHNOLOGY, "후측방 충돌방지 보조"),
+	SEA(INTELLIGENT_TECHNOLOGY, "안전 하차 보조"),
+	LKA(INTELLIGENT_TECHNOLOGY, "차량 이탈방지 보조"),
+
+	LED_HEADLAMP(INTERIOR_OR_EXTERIOR, "LED 헤드램프"),
+	ROOF_RACK(INTERIOR_OR_EXTERIOR, "루프 랙"),
+	SUN_ROOF(INTERIOR_OR_EXTERIOR, "선루프"),
+
+	VENTILATION_SEAT(SEAT, "동승석 통풍시트"),
+	IMS(SEAT, "운전석 자세 메모리 시스템"),
+	HEATED_SEAT(SEAT, "뒷좌석 열선시트"),
+
+	REAR_MONITOR(CONVENIENCE, "후방 모니터"),
+	BLIND_SPOT_VIEW_MONITOR(CONVENIENCE, "후측방 모니터"),
+	SURROUND_VIEW_MONITOR(CONVENIENCE, "서라운드 뷰 모니터"),
+
+	DISPLAY_AUDIO(MULTIMEDIA, "디스플레이 오디오"),
+	NAVIGATION(MULTIMEDIA, "내비게이션"),
+	PREMIUM_SOUND_SYSTEM(MULTIMEDIA, "프리미엄 사운드 시스템");
 
 	private final Category category;
 	private final String name;
