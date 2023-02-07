a0eb832df805379e0a570d6d3721face90f8079b
2je0 <bulljy@naver.com>
2023-02-07 11:22:27 +0900

    fix: deploy 위해서 파일 소소한 수정

--- front-end/src/pages/aboutus.ts
+++ front-end/src/pages/aboutus.ts
@@ -1,7 +1,13 @@
+import { OptionForm } from '@/components/OptionForm/OptionForm';
 import Component from '@/core/Component';
+import { qs } from '@/utils/querySelector';
 
 export class AboutUs extends Component {
   template(): string {
-    return `<div>about us page</div>`;
+    return `<div id="option-form">about us page</div>`;
+  }
+  mounted(): void {
+    const form = qs('#option-form', this.target) as HTMLDivElement;
+    new OptionForm(form);
   }
 }
