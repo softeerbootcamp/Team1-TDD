4d205a9fd12ad5b7d22ba1d68d951e9b86a597ca
MyuB <mjycoco0@naver.com>
2023-02-07 16:42:55 +0900

    chore: google maps api 사용환경 세팅

---
+++ front-end/src/utils/mapStyle.ts
@@ -0,0 +1,21 @@
+export function mapStyle() {
+  return [
+    {
+      featureType: "poi.business",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+  ];
+}
