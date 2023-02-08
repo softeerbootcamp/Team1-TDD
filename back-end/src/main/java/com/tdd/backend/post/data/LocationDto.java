b66b6ddae9b1d253fe32f1942ec683febccd69ba
jieonkim23 <kwldjs1023@gmail.com>
2023-02-08 17:22:43 +0900

    fix: @Getter 추가

--- back-end/src/main/java/com/tdd/backend/post/data/LocationDto.java
+++ back-end/src/main/java/com/tdd/backend/post/data/LocationDto.java
@@ -2,6 +2,9 @@ package com.tdd.backend.post.data;
 
 import com.tdd.backend.post.Location;
 
+import lombok.Getter;
+
+@Getter
 public class LocationDto {
 	private String latitude;		//위도
 	private String longitude;		//경도
