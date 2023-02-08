254b9e823746e024c7c4108ab59e03faf5ba5fdc
MyuB <mjycoco0@naver.com>
2023-02-08 21:42:30 +0900

    chore: 날짜 데이터 추가

---
+++ front-end/src/components/Calendar/constants.ts
@@ -0,0 +1,16 @@
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
