e407c24e04088546d65c1b65396ca2c1e069050a
jieonkim23 <kwldjs1023@gmail.com>
2023-02-08 15:15:56 +0900

    feat: LocationDto(latitude, longitude) 생성

---
+++ back-end/src/main/java/com/tdd/backend/post/data/LocationDto.java
@@ -0,0 +1,16 @@
+package com.tdd.backend.post.data;
+
+import com.tdd.backend.post.Location;
+
+public class LocationDto {
+	private String latitude;		//위도
+	private String longitude;		//경도
+
+	public LocationDto(String latitude, String longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	public Location toEntity(){
+		return new Location(longitude, latitude);
+	}
+}
