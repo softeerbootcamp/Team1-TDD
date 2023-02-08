ceb32630d6858f5c0b3bfd5b0bfaa82e7d0506b3
jieonkim23 <kwldjs1023@gmail.com>
2023-02-08 17:50:00 +0900

    fix: @Valid requestbody 유효성 체크

--- back-end/src/main/java/com/tdd/backend/post/PostController.java
+++ back-end/src/main/java/com/tdd/backend/post/PostController.java
@@ -2,6 +2,8 @@ package com.tdd.backend.post;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +29,7 @@ public class PostController {
 	@PostMapping("/sharing")
 	@Operation(summary = "차량 공유하기 요청", description = "차량 공유하기 시 Post 정보가 insert 되어야 합니다.")
 	@ApiResponse(responseCode = "302", description = "메인 페이지로 REDIRECT")
-	public ResponseEntity<Void> shares(@RequestBody SharingDto sharingDto) {
+	public ResponseEntity<Void> shares(@RequestBody @Valid SharingDto sharingDto) {
 		// 요청: 차 이름, 위치, 옵션들, 동승여부, 운전경력, 사용자 id , 날짜 리스트,  요구사항
 		// 302 REDIRECT index.html
 		postService.save(sharingDto);
