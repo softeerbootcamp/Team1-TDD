63f892f48ef460fc2760aa6a95703ebcc2861773
River-Mt <rivermt1998@gmail.com>
2023-02-09 14:33:16 +0900

    feat: 차량과 옵션에 대한 관계를 데이터베이스화, 그에 따른 schema.sql 수정 및 data.sql 작성

--- back-end/src/main/resources/schema.sql
+++ back-end/src/main/resources/schema.sql
@@ -3,6 +3,9 @@ drop table if exists locations;
 drop table if exists appointments;
 drop table if exists posts;
 drop table if exists users;
+drop table if exists caroptions;
+drop table if exists cars;
+drop table if exists entireoptions;
 
 create table users
 (
@@ -16,12 +19,12 @@ create table users
 
 create table posts
 (
-    id           bigint auto_increment
+    id          bigint auto_increment
         primary key,
-    ride_option  varchar(100) not null,
-    car_name     varchar(100) not null,
-    user_id      bigint       not null,
-    requirement  text null,
+    ride_option varchar(100) not null,
+    car_name    varchar(100) not null,
+    user_id     bigint       not null,
+    requirement text         null,
     constraint posts_users_id_fk
         foreign key (user_id) references users (id)
             on update cascade on delete cascade
@@ -31,9 +34,9 @@ create table appointments
 (
     id        bigint auto_increment
         primary key,
-    date      date     not null,
+    date      date         not null,
     post_id   bigint       not null,
-    tester_id bigint null,
+    tester_id bigint       null,
     status    varchar(100) not null,
     constraint appointments_posts_id_fk
         foreign key (post_id) references posts (id)
@@ -46,9 +49,9 @@ create table appointments
 create table locations
 (
     longitude varchar(255) not null,
-    latitude varchar(255) not null,
-    post_id    bigint       not null,
-    id         bigint auto_increment
+    latitude  varchar(255) not null,
+    post_id   bigint       not null,
+    id        bigint auto_increment
         primary key,
     constraint locations_posts_id_fk
         foreign key (post_id) references posts (id)
@@ -65,4 +68,28 @@ create table options
     constraint options_posts_id_fk
         foreign key (post_id) references posts (id)
             on update cascade on delete cascade
+);
+
+create table cars
+(
+    id       bigint auto_increment primary key,
+    car_name varchar(100) not null
+);
+
+create table entireoptions
+(
+    id            bigint auto_increment primary key,
+    option_name   varchar(100) not null,
+    category_name varchar(100) not null
+);
+
+create table caroptions
+(
+    id        bigint auto_increment primary key,
+    car_id    bigint not null,
+    entireoption_id bigint not null,
+    constraint caroptions_cars_id_fk
+        foreign key (car_id) references cars (id) on update cascade on delete cascade,
+    constraint caroptions_entireoptions_id_fk
+        foreign key (entireoption_id) references entireoptions (id) on update cascade on delete cascade
 );
