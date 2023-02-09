9359d0f1302118a0fbcaa1bd46a0a9388f4f5edf
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-02-09 20:46:24 +0900

    feature(#84) Enum으로 특정 차의 옵션들을 데이터베이스 화 (#94)
    * feat: 차량과 옵션에 대한 관계를 데이터베이스화, 그에 따른 schema.sql 수정 및 data.sql 작성
    
    * refactor: sql문 리팩토링
    
    * feat: 기존 enum으로 구현한 특정 차량의 옵션 정보를 데이터베이스 화 하여 모델 및 레포지토리 구현
    
    * feat: 차량 이름을 통해 차량 id를 얻어오기 위한 차 도메인 및 레포지토리 구현
    
    * refactor: enum을 데이터베이스화 해서 발생한 변동사항 적용
    
    * refactor: echo dockercomose.yml 삭제
    
    * refactor: enum으로 있었던 특정 차의 옵션 클래스 삭제
    
    * refactor: 디렉토리 구조 변경
    
    * refactor: 메서드 이름 변경
    
    * refactor: 부적절한 메서드 이름 변경

--- back-end/src/main/resources/schema.sql
+++ back-end/src/main/resources/schema.sql
@@ -3,6 +3,9 @@ drop table if exists locations;
 drop table if exists appointments;
 drop table if exists posts;
 drop table if exists users;
+drop table if exists car_options;
+drop table if exists cars;
+drop table if exists entire_options;
 
 create table users
 (
@@ -16,12 +19,12 @@ create table users
 
 create table posts
 (
-    id           bigint auto_increment
+    id          bigint auto_increment
         primary key,
-    ride_option  varchar(100) not null,
-    car_name     varchar(100) not null,
-    user_id      bigint       not null,
-    requirement  text null,
+    ride_option varchar(100) not null,
+    car_name    varchar(100) not null,
+    user_id     bigint       not null,
+    requirement text         null,
     constraint posts_users_id_fk
         foreign key (user_id) references users (id)
             on update cascade on delete cascade
@@ -31,9 +34,9 @@ create table appointments
 (
     id        bigint auto_increment
         primary key,
-    date      date     not null,
+    date      date         not null,
     post_id   bigint       not null,
-    tester_id bigint null,
+    tester_id bigint       null,
     status    varchar(100) not null,
     constraint appointments_posts_id_fk
         foreign key (post_id) references posts (id)
@@ -46,9 +49,9 @@ create table appointments
 create table locations
 (
     longitude varchar(255) not null,
-    latitude varchar(255) not null,
-    post_id    bigint       not null,
-    id         bigint auto_increment
+    latitude  varchar(255) not null,
+    post_id   bigint       not null,
+    id        bigint auto_increment
         primary key,
     constraint locations_posts_id_fk
         foreign key (post_id) references posts (id)
@@ -65,4 +68,28 @@ create table options
     constraint options_posts_id_fk
         foreign key (post_id) references posts (id)
             on update cascade on delete cascade
+);
+
+create table cars
+(
+    id       bigint auto_increment primary key,
+    car_name varchar(100) not null
+);
+
+create table entire_options
+(
+    id            bigint auto_increment primary key,
+    option_name   varchar(100) not null,
+    category_name varchar(100) not null
+);
+
+create table car_options
+(
+    car_id    bigint not null,
+    entire_option_id bigint not null,
+    primary key (car_id, entire_option_id),
+    constraint car_options_cars_id_fk
+        foreign key (car_id) references cars (id) on update cascade on delete cascade,
+    constraint car_options_entire_options_id_fk
+        foreign key (entire_option_id) references entire_options (id) on update cascade on delete cascade
 );
