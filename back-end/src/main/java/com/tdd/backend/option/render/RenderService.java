8bf5b3341856f3c79a07880b743c16a48d970b2a
River-Mt <rivermt1998@gmail.com>
2023-02-09 18:24:13 +0900

    refactor: enum을 데이터베이스화 해서 발생한 변동사항 적용

--- back-end/src/main/java/com/tdd/backend/option/render/RenderService.java
+++ back-end/src/main/java/com/tdd/backend/option/render/RenderService.java
@@ -6,22 +6,34 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.tdd.backend.option.Category;
-import com.tdd.backend.option.EntireOption;
 import com.tdd.backend.option.data.OptionDto;
 import com.tdd.backend.option.data.OptionResponse;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
 @Service
 public class RenderService {
+
+	private final RenderRepository renderRepository;
+	private final CarRepository carRepository;
+
 	public List<OptionResponse> getOptions(String carName) {
-		List<EntireOption> entireOptionList = SpecificCarOption.getCar(carName).getOptions();
+		Long carId = carRepository.findIdByCarName(carName).orElseThrow(IllegalArgumentException::new);
+		log.info("CarId : " + carId);
+
+		List<RenderOption> renderOptionList = renderRepository.getCarIdByCarName(carId);
 		List<OptionResponse> response = new ArrayList<>();
 
 		for (Category category : Category.values()) {
 			List<OptionDto> optionDtoList = new ArrayList<>();
 
-			for (EntireOption renderOption : entireOptionList) {
+			for (RenderOption renderOption : renderOptionList) {
+				log.info("category : " + renderOption.getCategory());
 				if (isCategoryEquals(category, renderOption.getCategory())) {
-					OptionDto optionDto = new OptionDto(renderOption.getName(), renderOption.getCategory().getName());
+					OptionDto optionDto = new OptionDto(renderOption.getOptionName(), renderOption.getCategory());
 					optionDtoList.add(optionDto);
 				}
 			}
@@ -34,8 +46,8 @@ public class RenderService {
 		return response;
 	}
 
-	private boolean isCategoryEquals(Category categoryA, Category categoryB) {
-		return categoryA.equals(categoryB);
+	private boolean isCategoryEquals(Category categoryA, String categoryB) {
+		return categoryA.getName().equals(categoryB);
 	}
 
 }
