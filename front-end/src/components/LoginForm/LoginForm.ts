dfae0b990453a1e72024d002d8d0c32964eaaba4
2je0 <bulljy@naver.com>
2023-02-09 20:39:23 +0900

    fix(#93): login form 에서 footer 제거

--- front-end/src/components/LoginForm/LoginForm.ts
+++ front-end/src/components/LoginForm/LoginForm.ts
@@ -47,15 +47,6 @@ export class LoginForm extends Component {
                 </div>
             </div>
         </div>
-        
-        <footer>
-            <p>
-                Created with <i class="fa fa-heart"></i> by
-                <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-                - Read how I created this and how you can join the challenge
-                <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-            </p>
-        </footer>
         `;
   }
   setEvent(): void {
