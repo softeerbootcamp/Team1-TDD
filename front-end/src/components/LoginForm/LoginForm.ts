78f5ecb3439c79d7a1ae0919bec1bb839b53556b
2je0 <bulljy@naver.com>
2023-02-11 00:50:33 +0900

    fix: axios api 주소 ec2로 변경

--- front-end/src/components/LoginForm/LoginForm.ts
+++ front-end/src/components/LoginForm/LoginForm.ts
@@ -60,7 +60,7 @@ export class LoginForm extends Component {
     });
     this.addEvent('click', '#api-test', () => {
       axios
-        .get(`http://localhost:8080/options/sonata`)
+        .get(`http://52.78.106.53:8080/options/sonata`)
         .then((data) => console.log(data))
         .catch((error) => console.log(error));
     });
@@ -71,7 +71,7 @@ export class LoginForm extends Component {
       const enteredEmail = $email.value;
       const enteredPassword = $password.value;
       axios
-        .post(`http://localhost:8080/login`, {
+        .post(`http://52.78.106.53:8080/login`, {
           email: enteredEmail,
           userPassword: enteredPassword,
         })
@@ -91,7 +91,7 @@ export class LoginForm extends Component {
       const enteredName = $name.value;
       const enteredPassword = $password.value;
       axios
-        .post(`http://localhost:8080/users`, {
+        .post(`http://52.78.106.53:8080/users`, {
           email: enteredEmail,
           phoneNumber: enteredTel,
           userName: enteredName,
