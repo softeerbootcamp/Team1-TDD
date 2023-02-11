53cf61e3b741d275749c37b4c1a1e1640c9c49e6
jieonkim23 <kwldjs1023@gmail.com>
2023-02-12 04:24:36 +0900

    feat: 시승하기 최종 예약 컨트롤러 맵핑 구현, sendAllData는 DrivingResponse로 모든 데이터 응답하도록 변경

--- back-end/src/main/java/com/tdd/backend/post/controller/DrivingController.java
+++ back-end/src/main/java/com/tdd/backend/post/controller/DrivingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.tdd.backend.post.data.SharingDto;
+import com.tdd.backend.post.data.DrivingResponse;
 import com.tdd.backend.post.service.DrivingService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,10 +21,13 @@ public class DrivingController {
 
 	@GetMapping("/test-driving/{postId}")
 	@Operation(summary = "location 리스트에서 하나의 포스트 선택 시에 대한 요청", description = "포스트의 해당 차종과 옵션 목록으로 응답해야 함.")
-	public ResponseEntity<SharingDto> sendSharingDataByPostId(@PathVariable Long postId) {
+	public ResponseEntity<DrivingResponse> sendAllDataByPostId(@PathVariable Long postId) {
 		//TODO: pending 확인하고 아니면 다른 응답 보내야함
-		SharingDto sharingDto = drivingService.getSharingDateByPostId(postId);
-		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(sharingDto);
+		DrivingResponse drivingResponse = drivingService.getAllDataByPostId(postId);
+		return ResponseEntity
+			.ok()
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(drivingResponse);
 	}
 
 	@PostMapping("/test-driving/")
@@ -40,8 +43,9 @@ public class DrivingController {
 
 	@PatchMapping("/appointments/{appointmentId}")
 	@Operation(summary = "최종적인 예약 요청", description = "시승하기에 대한 사용자의 최종적인 요청으로 Appointment의 상태를 승낙으로 Update해야 함.")
-	public void requestAppointment(@PathVariable Long appointmentId) {
-		// 요청 : appointment id
+	public ResponseEntity requestAppointment(@PathVariable Long appointmentId) {
+		drivingService.approveAppointment(appointmentId);
+		return ResponseEntity.ok().build();
 	}
 
 	@GetMapping("/test-driving/appointments/{postId}")
