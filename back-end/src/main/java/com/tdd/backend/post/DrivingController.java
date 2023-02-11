4e24a8bd54622db5fe23da2bfdb5d242818dd524
jieonkim23 <kwldjs1023@gmail.com>
2023-02-12 02:24:34 +0900

    refactor: 필요한 api 스웨거 정보 수정

--- back-end/src/main/java/com/tdd/backend/post/DrivingController.java
+++ back-end/src/main/java/com/tdd/backend/post/DrivingController.java
@@ -4,7 +4,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tdd.backend.post.data.SharingDto;
 
@@ -16,8 +18,16 @@ import lombok.RequiredArgsConstructor;
 public class DrivingController {
 	private final DrivingService drivingService;
 
-	@GetMapping("/test-driving/locations")
-	@Operation(summary = "시승가능한 차량 요청", description = "차종과 선택한 옵션 리스트를 요청 받으면 해당하는 Post의 location 리스트로 응답해야 함.")
+	@GetMapping("/test-driving/{postId}")
+	@Operation(summary = "location 리스트에서 하나의 포스트 선택 시에 대한 요청", description = "포스트의 해당 차종과 옵션 목록으로 응답해야 함.")
+	public ResponseEntity<SharingDto> sendSharingDataByPostId(@PathVariable Long postId) {
+		//TODO: pending 확인하고 아니면 다른 응답 보내야함
+		SharingDto sharingDto = drivingService.getSharingDateByPostId(postId);
+		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(sharingDto);
+	}
+
+	@PostMapping("/test-driving/")
+	@Operation(summary = "시승가능한 차량 공유에 대한 모든 정보 요청", description = "차종과 가능한 날짜 리스트, 선택한 옵션 리스트를 요청 받으면 해당하는 Post의 관련 모든 정보를 응답함.")
 	public void sendSharingDataByOptions() {
 		// 요청: 차 이름, 날짜리스트, 옵션 리스트
 		// 차이름: 필수
@@ -27,15 +37,10 @@ public class DrivingController {
 
 	}
 
-	@GetMapping("/test-driving/{postId}")
-	@Operation(summary = "location 리스트에서 하나의 포스트 선택 시에 대한 요청", description = "포스트의 해당 차종과 옵션 목록으로 응답해야 함.")
-	public ResponseEntity<SharingDto> sendSharingDataByPostId(@PathVariable Long postId) {
-		//TODO: pending 확인하고 아니면 다른 응답 보내야함
-		SharingDto sharingDto = drivingService.getSharingDateByPostId(postId);
-		return ResponseEntity
-			.ok()
-			.contentType(MediaType.APPLICATION_JSON)
-			.body(sharingDto);
+	@PatchMapping("/appointments/{appointmentId}")
+	@Operation(summary = "최종적인 예약 요청", description = "시승하기에 대한 사용자의 최종적인 요청으로 Appointment의 상태를 승낙으로 Update해야 함.")
+	public void requestAppointment(@PathVariable Long appointmentId) {
+		// 요청 : appointment id
 	}
 
 	@GetMapping("/test-driving/appointments/{postId}")
