ca40866298507e4492860a336ea9f191b1c56b6e
jieonkim23 <kwldjs1023@gmail.com>
2023-02-12 02:15:26 +0900

    feat: DrivingService 생성, post id로 sharing data 받아오는 서비스 메소드 추가

---
+++ back-end/src/main/java/com/tdd/backend/post/DrivingService.java
@@ -0,0 +1,38 @@
+package com.tdd.backend.post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.tdd.backend.option.data.OptionDto;
+import com.tdd.backend.post.data.SharingDto;
+import com.tdd.backend.post.model.Location;
+import com.tdd.backend.post.model.Option;
+import com.tdd.backend.post.model.Post;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DrivingService {
+
+	private final PostRepository postRepository;
+
+	public SharingDto getSharingDateByPostId(Long postId) {
+		Optional<Post> post = postRepository.findById(postId);
+		List<Option> options = postRepository.findOptionByPostId(postId);
+		List<String> dates = postRepository.findAppointmentByPostId(postId);
+		Optional<Location> location = postRepository.findLocationByPostId(postId);
+		List<OptionDto> optionDtos = options.stream().map(Option::toDto).collect(Collectors.toList());
+		return SharingDto.builder()
+			.post(post.orElseThrow(RuntimeException::new).toDto())
+			.options(optionDtos)
+			.dates(dates)
+			.location(location.orElseThrow(RuntimeException::new).toDto())
+			.build();
+	}
+}
