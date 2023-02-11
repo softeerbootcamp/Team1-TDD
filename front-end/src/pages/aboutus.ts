4905626841bc89a4440cacc157324679a60a8195
2je0 <bulljy@naver.com>
2023-02-12 01:21:26 +0900

    fix(#104): 더미로 넣어놨던 about us의 login form 제거

--- front-end/src/pages/aboutus.ts
+++ front-end/src/pages/aboutus.ts
@@ -1,13 +1,8 @@
-import { LoginForm } from '@/components/LoginForm/LoginForm';
 import Component from '@/core/Component';
-import { qs } from '@/utils/querySelector';
 
 export class AboutUs extends Component {
   template(): string {
     return `<div id="option-form">about us page</div>`;
   }
-  mounted(): void {
-    const form = qs('#option-form', this.target) as HTMLDivElement;
-    new LoginForm(form);
-  }
+  mounted(): void {}
 }
