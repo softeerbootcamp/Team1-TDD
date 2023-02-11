05649efece70a2879bbf0dd97fba0c5dce4d721d
jieonkim23 <kwldjs1023@gmail.com>
2023-02-12 04:16:41 +0900

    refactor: 불필요한 필드 제거

--- back-end/src/main/java/com/tdd/backend/post/data/AppointmentDto.java
+++ back-end/src/main/java/com/tdd/backend/post/data/AppointmentDto.java
@@ -1,21 +1,16 @@
 package com.tdd.backend.post.data;
 
-import java.util.Date;
-
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class AppointmentDto {
-	private final Date date;
-	private final Long testerId;
-	private final String status;
+	private final Long id;
+	private final String date;
 
 	@Builder
-	private AppointmentDto(Date date, Long testerId, String status) {
+	public AppointmentDto(Long id, String date) {
+		this.id = id;
 		this.date = date;
-		this.testerId = testerId;
-		this.status = status;
 	}
-
 }
