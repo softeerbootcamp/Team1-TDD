bb49561e5b2992c98f0ac33de89bee59f097e278
jieonkim23 <kwldjs1023@gmail.com>
2023-02-12 02:01:04 +0900

    feat: post id로 option, appointment, location 찾는 쿼리 추가

--- back-end/src/main/java/com/tdd/backend/post/PostRepository.java
+++ back-end/src/main/java/com/tdd/backend/post/PostRepository.java
@@ -1,10 +1,25 @@
 package com.tdd.backend.post;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.tdd.backend.post.model.Location;
+import com.tdd.backend.post.model.Option;
 import com.tdd.backend.post.model.Post;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
+	@Query("SELECT latitude,longitude FROM locations WHERE post_id = :id")
+	Optional<Location> findLocationByPostId(@Param("id") Long id);
+
+	@Query("SELECT name, category FROM options WHERE  post_id = :id")
+	List<Option> findOptionByPostId(@Param("id") Long id);
+
+	@Query("SELECT  date FROM appointments WHERE post_id = :id")
+	List<String> findAppointmentByPostId(@Param("id") Long id);
 }
