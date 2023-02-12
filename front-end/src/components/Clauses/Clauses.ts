1d50bb77f2f714f2947ba3525fe21dbe81749581
MyuB <52685740+MyuB@users.noreply.github.com>
2023-02-12 21:02:50 +0900

    refactor(#112) 약관페이지 router에 따른 text 수정 및 리팩토링 (#120)
    * fix: template에서 href의 상태에 따라 다른 text를 보기 위한 마크업 수정
    
    * refactor: href의 상태를 state에 담도록 변경
    
    * refactor: component방식에 맞게 이벤트 등록 수정

--- front-end/src/components/Clauses/Clauses.ts
+++ front-end/src/components/Clauses/Clauses.ts
@@ -4,34 +4,39 @@ import styles from "./Clauses.module.scss";
 import { qs, qsa } from "@/utils/querySelector";
 
 export class Clauses extends Component {
+  setup(): void {
+    const status = document.location.href as string;
+    if (status.includes("sharing")) {
+      this.state.welcomeMessage = "소중한 경험을 공유해주셔서 감사합니다.";
+      this.state.status = "공유해주시기에 앞서, ";
+    } else {
+      this.state.welcomeMessage = "Experiences Begin Here";
+      this.state.status = "경험하기에 앞서, ";
+    }
+  }
   template(): string {
-    return literal();
+    return literal(this.state.welcomeMessage, this.state.status);
   }
 
-  mounted(): void {
-    const form = qs("form")! as HTMLFormElement;
-    form.addEventListener("submit", (e: SubmitEvent) => e.preventDefault());
-    const checkbox = qsa("input")! as NodeListOf<HTMLInputElement>;
-
-    checkbox.forEach((box) => {
-      box.addEventListener("click", (_) => {
-        const nextBtn = qs(`.${styles["next-button"]}`)! as HTMLButtonElement;
-        if (allChecked()) {
-          nextBtn.disabled = false;
-          nextBtn.classList.toggle("active");
-        } else {
-          nextBtn.disabled = true;
-          nextBtn.classList.toggle("active");
-        }
-      });
-    });
-
-    function allChecked() {
-      const checkbox = qsa("input")! as NodeListOf<HTMLInputElement>;
-      for (const check of checkbox) {
-        if (!check.checked) return false;
+  setEvent(): void {
+    this.addEvent("submit", "form", (e) => e.preventDefault());
+    this.addEvent("click", "input", (_) => {
+      const nextBtn = qs(`.${styles["next-button"]}`)! as HTMLButtonElement;
+      if (this.isAllChecked()) {
+        nextBtn.disabled = false;
+        nextBtn.classList.toggle("active");
+      } else {
+        nextBtn.disabled = true;
+        nextBtn.classList.toggle("active");
       }
-      return true;
+    });
+  }
+
+  isAllChecked() {
+    const checkbox = qsa("input")! as NodeListOf<HTMLInputElement>;
+    for (const check of checkbox) {
+      if (!check.checked) return false;
     }
+    return true;
   }
 }
