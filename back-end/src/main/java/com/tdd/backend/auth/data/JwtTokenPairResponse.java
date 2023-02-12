48a9247a055ab494bdbbf12215114e4583818729
win_min <psm9718@gmail.com>
2023-02-13 06:26:02 +0900

    fix: 오타 수정

--- back-end/src/main/java/com/tdd/backend/auth/data/JwtTokenPairResponse.java
+++ back-end/src/main/java/com/tdd/backend/auth/data/JwtTokenPairResponse.java
@@ -3,7 +3,7 @@ package com.tdd.backend.auth.data;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter //https://stackoverflow.com/questions/28466207/could-not-find-acceptable-representation-using-spring-boot-starter-web
+@Getter
 public class JwtTokenPairResponse {
 	private final String accessToken;
 	private final String refreshToken;
--- back-end/src/main/java/com/tdd/backend/auth/exception/ExpiredATKException.java
+++ back-end/src/main/java/com/tdd/backend/auth/exception/ExpiredATKException.java
@@ -1,7 +1,7 @@
 package com.tdd.backend.auth.exception;
 
 public class ExpiredATKException extends RuntimeException {
-	private static final String MESSAGE = "액세스 토큰이 만료되었습니다. 재발급해주세요.";
+	private static final String MESSAGE = "토큰이 만료되었습니다. 재발급해주세요.";
 
 	public ExpiredATKException() {
 		super(MESSAGE);
