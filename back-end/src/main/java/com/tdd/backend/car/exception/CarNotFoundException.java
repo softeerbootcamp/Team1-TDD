4f9d95d0412b8c8dfab254e9afdfde7ccba81724
River-Mt <rivermt1998@gmail.com>
2023-02-13 02:23:47 +0900

    refactor: CarNotFoundException 에러 메시지 수정

--- back-end/src/main/java/com/tdd/backend/car/exception/CarNotFoundException.java
+++ back-end/src/main/java/com/tdd/backend/car/exception/CarNotFoundException.java
@@ -1,7 +1,7 @@
 package com.tdd.backend.car.exception;
 
 public class CarNotFoundException extends RuntimeException {
-	private static final String MESSAGE = "인증되지 않은 접근입니다.";
+	private static final String MESSAGE = "존재하지 않는 차 입니다.";
 
 	public CarNotFoundException() {
 		super(MESSAGE);
