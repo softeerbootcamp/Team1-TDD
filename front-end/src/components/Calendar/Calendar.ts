173c24b5c7d1d9cb131126d22d482cd416f964df
2je0 <bulljy@naver.com>
2023-02-14 00:12:20 +0900

    style: 캘린더 재 디자인

--- front-end/src/components/Calendar/Calendar.ts
+++ front-end/src/components/Calendar/Calendar.ts
@@ -1,8 +1,8 @@
-import Component from "@/core/Component";
-import styles from "./Calendar.module.scss";
-import { qs, qsa } from "@/utils/querySelector";
-import { months, daysOfWeek } from "./constants";
-import { leftBtn, rightBtn } from "./icon";
+import Component from '@/core/Component';
+import styles from './Calendar.module.scss';
+import { qs, qsa } from '@/utils/querySelector';
+import { months, daysOfWeek } from './constants';
+import { leftBtn, rightBtn } from './icon';
 
 let date = new Date();
 
@@ -13,31 +13,31 @@ export class Calendar extends Component {
 
   template(): string {
     return `
-      <div id="${styles["calendar"]}"></div>
+      <div id="${styles['calendar']}"></div>
       `;
   }
 
   setEvent(): void {
-    this.addEvent("click", "#prev", this.movePrevMonth.bind(this));
-    this.addEvent("click", "#next", this.moveNextMonth.bind(this));
-    this.addEvent("change", "#year-select", this.selectYear.bind(this));
-    this.addEvent("click", "td", (e) => {
+    this.addEvent('click', '#prev', this.movePrevMonth.bind(this));
+    this.addEvent('click', '#next', this.moveNextMonth.bind(this));
+    this.addEvent('change', '#year-select', this.selectYear.bind(this));
+    this.addEvent('click', 'td', (e) => {
       const target = e.target as HTMLTableCellElement;
-      if (!target.classList.contains(`${styles["active"]}`)) {
-        target.classList.add(`${styles["active"]}`);
+      if (!target.classList.contains(`${styles['active']}`)) {
+        target.classList.add(`${styles['active']}`);
         this.props.dates.push(
-          `${date.getFullYear()}/${("0" + date.getMonth()).slice(-2)}/${(
-            "0" + target.innerText
+          `${date.getFullYear()}/${('0' + date.getMonth()).slice(-2)}/${(
+            '0' + target.innerText
           ).slice(-2)}`
         );
         console.log(this.props);
       } else {
-        target.classList.remove(`${styles["active"]}`);
+        target.classList.remove(`${styles['active']}`);
         this.props.dates = this.props.dates.filter(
           (ele: string) =>
             ele !==
-            `${date.getFullYear()}/${("0" + date.getMonth()).slice(-2)}/${(
-              "0" + target.innerText
+            `${date.getFullYear()}/${('0' + date.getMonth()).slice(-2)}/${(
+              '0' + target.innerText
             ).slice(-2)}`
         );
         console.log(this.props.dates);
@@ -46,7 +46,7 @@ export class Calendar extends Component {
   }
 
   markDates() {
-    const yearMonth = `${date.getFullYear()}/${("0" + date.getMonth()).slice(
+    const yearMonth = `${date.getFullYear()}/${('0' + date.getMonth()).slice(
       -2
     )}`;
     const matchedDates = this.props.dates.filter((ele: string) =>
@@ -55,12 +55,12 @@ export class Calendar extends Component {
     const trimedDates: string[] = matchedDates
       .map((day: string) => day.slice(-2))
       .sort();
-    const tds = qsa("td") as NodeListOf<HTMLTableCellElement>;
+    const tds = qsa('td') as NodeListOf<HTMLTableCellElement>;
     let datesIdx = 0;
     for (let i = 0; i < tds.length; i++) {
       if (datesIdx === trimedDates.length) break;
       if (tds[i].innerText === trimedDates[datesIdx]) {
-        tds[i].classList.add(`${styles["active"]}`);
+        tds[i].classList.add(`${styles['active']}`);
         datesIdx++;
       }
     }
@@ -81,7 +81,7 @@ export class Calendar extends Component {
   }
 
   selectYear() {
-    const year = qs("#year-select") as HTMLOptionElement;
+    const year = qs('#year-select') as HTMLOptionElement;
     date.setFullYear(parseInt(year.value));
     this.renderCalendar();
     this.markDates();
@@ -96,37 +96,37 @@ export class Calendar extends Component {
     let month = date.getMonth();
 
     let header = `
-        <div id="${styles["month-header"]}">
-          <div id="prev">${leftBtn}</div>
+        <div id="${styles['month-header']}">
+          <div id="prev" class="${styles.prev}"></div>
           <h2>${months[month]} ${year}</h2>
-          <div id="next">${rightBtn}</div>
+          <div id="next" class="${styles.next}"></div>
         </div>
         <table id="calendar">
           <thead>
             <tr>
-              ${daysOfWeek.map((day) => `<th>${day}</th>`).join("")}
+              ${daysOfWeek.map((day) => `<th>${day}</th>`).join('')}
             </tr>
           </thead>
           <tbody>
             ${this.generateCalendar(date)}
           </tbody>
         </table>
-        <div class=${styles["year-wrapper"]}>
-          <div id=${styles["year-selector"]}>
+        <div class=${styles['year-wrapper']}>
+          <div id=${styles['year-selector']}>
             <select id="year-select">
-              ${Array.from({ length: 50 }, (_, i) => year - 25 + i)
+              ${Array.from({ length: 10 }, (_, i) => year - 5 + i)
                 .map(
                   (y) =>
                     `<option value="${y}" ${
-                      y === year ? "selected" : ""
+                      y === year ? 'selected' : ''
                     }>${y}</option>`
                 )
-                .join("")}
+                .join('')}
             </select>
           </div>
         </div>
       `;
-    qs(`#${styles["calendar"]}`)!.innerHTML = header;
+    qs(`#${styles['calendar']}`)!.innerHTML = header;
     this.markDates();
   }
 
@@ -135,22 +135,22 @@ export class Calendar extends Component {
     let month = date.getMonth();
     let firstDayOfMonth = new Date(year, month, 1).getDay();
     let daysInMonth = this.getDaysInMonth(year, month);
-    let html = "";
+    let html = '';
     let week = 0;
     let day = 1;
     while (day <= daysInMonth) {
-      html += "<tr>";
+      html += '<tr>';
       for (let i = 0; i < 7; i++) {
         if (week === 0 && i < firstDayOfMonth) {
-          html += "<td></td>";
+          html += '<td></td>';
         } else if (day <= daysInMonth) {
-          html += `<td>${("0" + day).slice(-2)}</td>`;
+          html += `<td>${('0' + day).slice(-2)}</td>`;
           day++;
         } else {
-          html += "<td></td>";
+          html += '<td></td>';
         }
       }
-      html += "</tr>";
+      html += '</tr>';
       week++;
     }
     return html;
