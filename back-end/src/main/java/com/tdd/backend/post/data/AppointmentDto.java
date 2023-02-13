4f660b8174e82e6b798d36b26a7f06247df38f7f
jieonkim23 <kwldjs1023@gmail.com>
2023-02-13 21:00:21 +0900

    feat: AppointmentDto에 status 필드 추가

--- back-end/src/main/java/com/tdd/backend/post/data/AppointmentDto.java
+++ back-end/src/main/java/com/tdd/backend/post/data/AppointmentDto.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 public class AppointmentDto {
 	private final Long id;
 	private final String date;
+	private final String status;
 
 	@Builder
-	public AppointmentDto(Long id, String date) {
+	public AppointmentDto(Long id, String date, String status) {
 		this.id = id;
 		this.date = date;
+		this.status = status;
 	}
 }
