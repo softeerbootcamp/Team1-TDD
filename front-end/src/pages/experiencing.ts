bb852659714f2ace9d14ea4db1a374b4f32abfdc
2je0 <bulljy@naver.com>
2023-02-13 22:02:34 +0900

    style: 경험하기 페이지 컴포넌트 배치
    차종, 캘린더, 옵션, 지도

--- front-end/src/pages/experiencing.ts
+++ front-end/src/pages/experiencing.ts
@@ -1,23 +1,32 @@
+import { Calendar } from '@/components/Calendar/Calendar';
+import { ExperienceMap } from '@/components/ExperienceMap/ExperienceMap';
 import { ImageSlider } from '@/components/ImageSlider/ImageSlider';
 import { OptionForm } from '@/components/OptionForm/OptionForm';
 import { carList } from '@/constants/carList';
 import Component from '@/core/Component';
 import { qs } from '@/utils/querySelector';
+import styles from '@/styles/experiencing.module.scss';
 
 export class Experiencing extends Component {
   template(): string {
     return `
-    <div id="imageSlider"></div>
-    <div id="optionSelector"></div>
+    <div id="ex-imageSlider"></div>
+    <div class="${styles['option-calendar-container']}">
+      <div id="ex-optionSelector"></div>
+      <div id="ex-calendar"></div>
+    </div>
+    <div id="ex-map"></div>
     `;
   }
   mounted(): void {
-    const $imageSlider = qs('#imageSlider', this.target) as HTMLDivElement;
-    const $optionSelector = qs(
-      '#optionSelector',
-      this.target
-    ) as HTMLDivElement;
-    new ImageSlider($imageSlider, { list: carList });
-    new OptionForm($optionSelector);
+    const $imageSlider = qs('#ex-imageSlider', this.target);
+    const $optionSelector = qs('#ex-optionSelector', this.target);
+    const $calendar = qs('#ex-calendar', this.target);
+    const $map = qs('#ex-map', this.target);
+
+    new ImageSlider($imageSlider as HTMLDivElement, { list: carList });
+    new OptionForm($optionSelector as HTMLDivElement);
+    new Calendar($calendar as HTMLDivElement);
+    new ExperienceMap($map as HTMLDivElement);
   }
 }
