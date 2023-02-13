57875c176a3d949854f0cf51f2c25bbfdc2b343e
jieonkim23 <kwldjs1023@gmail.com>
2023-02-13 20:59:54 +0900

    feat: MyPage에서 사용할 시승정보를 담을 DrivingInfo 생성

---
+++ back-end/src/main/java/com/tdd/backend/myPage/data/DrivingInfo.java
@@ -0,0 +1,15 @@
+package com.tdd.backend.myPage.data;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class DrivingInfo {
+	private final DefaultInfo post;
+	private final String date;
+	@Builder
+	public DrivingInfo(DefaultInfo post, String date) {
+		this.post = post;
+		this.date = date;
+	}
+}
