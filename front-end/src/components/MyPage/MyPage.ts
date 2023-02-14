a6480d454a91848fc27038a16b4c19848f01c411
MyuB <mjycoco0@naver.com>
2023-02-14 12:35:34 +0900

    fix: mypage에 bulletinboard component 연동

--- front-end/src/components/MyPage/MyPage.ts
+++ front-end/src/components/MyPage/MyPage.ts
@@ -1,12 +1,28 @@
 import Component from "@/core/Component";
+import { qsa } from "@/utils/querySelector";
 import { literal } from "./template";
+import styles from "./MyPage.module.scss";
+// import styles from "../BulletinBoard/BulletinBoard.module.scss";
+import { BulletinBoard } from "../BulletinBoard/BulletinBoard";
 
 export class MyPage extends Component {
-  setup(): void {}
+  setup(): void {
+    console.log(styles);
+  }
 
   template(): string {
     return literal();
   }
 
-  mounted(): void {}
+  mounted(): void {
+    const boards = qsa(`.${styles["board-wrapper"]}`);
+    for (const board of boards) {
+      const bd = board as HTMLElement;
+      new BulletinBoard(bd, {
+        module: {
+          css: styles,
+        },
+      });
+    }
+  }
 }
