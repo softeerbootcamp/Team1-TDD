c2f8df2fe0abfe17bcead7cdabe814569182806a
MyuB <mjycoco0@naver.com>
2023-02-14 17:04:50 +0900

    fix: get 할때 token 추가

--- front-end/src/apis/mypage.ts
+++ front-end/src/apis/mypage.ts
@@ -1,8 +1,9 @@
 import { axiosInstance } from ".";
 
 export const getUserInfo = async () => {
-  const accessToken = localStorage.getItem("accessToken");
-  return axiosInstance.get("/test/auth", {
-    headers: { Authorization: accessToken },
+  return axiosInstance.get("/mypage", {
+    headers: {
+      Authorization: localStorage.getItem("accessToken"),
+    },
   });
 };
