28b4a5d4a320d830e9e22cf6d066e3248272cdad
MyuB <mjycoco0@naver.com>
2023-02-14 17:12:20 +0900

    refactor: state로 받은 props를 변수로 받아 사용

--- front-end/src/components/BulletinBoard/BulletinBoard.ts
+++ front-end/src/components/BulletinBoard/BulletinBoard.ts
@@ -1,17 +1,13 @@
 import Component from "@/core/Component";
-// import styles from "./BulletinBoard.module.scss";
 
 export class BulletinBoard extends Component {
-  setup(): void {
-    this.state.styles = this.props.module.css;
-  }
-
   template(): string {
+    const styles = this.props.module.css;
     return `
-      <div class="${this.state.styles["board"]}">
-        <div class="${this.state.styles["card-wrapper"]}">
-          <div class="${this.state.styles["image-wrapper"]}"></div>
-          <div class="${this.state.styles["text-wrapper"]}"></div>
+      <div class="${styles["board"]}">
+        <div class="${styles["card-wrapper"]}">
+          <div class="${styles["image-wrapper"]}"></div>
+          <div class="${styles["text-wrapper"]}"></div>
         </div>
       </div>
     `;
