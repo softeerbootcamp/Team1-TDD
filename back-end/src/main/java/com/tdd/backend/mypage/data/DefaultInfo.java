aa3ada60903cc4b8fc36d7922458a49a31fc4905
jieonkim23 <kwldjs1023@gmail.com>
2023-02-15 18:32:39 +0900

    feat: DefaultInfo 에 location 필드 추가

--- back-end/src/main/java/com/tdd/backend/mypage/data/DefaultInfo.java
+++ back-end/src/main/java/com/tdd/backend/mypage/data/DefaultInfo.java
@@ -3,6 +3,7 @@ package com.tdd.backend.mypage.data;
 import java.util.List;
 
 import com.tdd.backend.car.data.OptionDto;
+import com.tdd.backend.post.model.Location;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -13,14 +14,17 @@ public class DefaultInfo {
 	private final String carName;
 	private final String rideOption;
 	private final String requirement;
+	private final Location location;
 	private final List<OptionDto> options;
 
 	@Builder
-	private DefaultInfo(Long id, String carName, String rideOption, String requirement, List<OptionDto> options) {
+	private DefaultInfo(Long id, String carName, String rideOption, String requirement, Location location,
+		List<OptionDto> options) {
 		this.id = id;
 		this.carName = carName;
 		this.rideOption = rideOption;
 		this.requirement = requirement;
+		this.location = location;
 		this.options = options;
 	}
 }
