6bd641f90822f727c7a5ded873c243e5b18cf8ca
jieonkim23 <kwldjs1023@gmail.com>
2023-02-15 18:45:43 +0900

    fix: DefaultInfo 에 location -> locationDto로 변경

--- back-end/src/main/java/com/tdd/backend/mypage/data/DefaultInfo.java
+++ back-end/src/main/java/com/tdd/backend/mypage/data/DefaultInfo.java
@@ -3,7 +3,7 @@ package com.tdd.backend.mypage.data;
 import java.util.List;
 
 import com.tdd.backend.car.data.OptionDto;
-import com.tdd.backend.post.model.Location;
+import com.tdd.backend.post.data.LocationDto;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -14,11 +14,11 @@ public class DefaultInfo {
 	private final String carName;
 	private final String rideOption;
 	private final String requirement;
-	private final Location location;
+	private final LocationDto location;
 	private final List<OptionDto> options;
 
 	@Builder
-	private DefaultInfo(Long id, String carName, String rideOption, String requirement, Location location,
+	private DefaultInfo(Long id, String carName, String rideOption, String requirement, LocationDto location,
 		List<OptionDto> options) {
 		this.id = id;
 		this.carName = carName;
