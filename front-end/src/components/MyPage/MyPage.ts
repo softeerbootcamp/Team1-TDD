7dc646c5d2d3e9ad15a3458b53f1bba0d2d36eb3
MyuB <mjycoco0@naver.com>
2023-02-15 10:26:02 +0900

    fix: userInfo를 받아오는 부분을 각각의 변수로 설정

--- front-end/src/components/MyPage/MyPage.ts
+++ front-end/src/components/MyPage/MyPage.ts
@@ -30,27 +30,21 @@ export class MyPage extends Component {
   }
 
   mounted(): void {
-    const expBoard = qs(`.${styles["exp-board"]}`) as HTMLDivElement;
-    const shareBoard = qs(`.${styles["share-board"]}`) as HTMLDivElement;
+    const $expBoard = qs(`.${styles["exp-board"]}`) as HTMLDivElement;
+    const $shareBoard = qs(`.${styles["share-board"]}`) as HTMLDivElement;
   }
 
   setUserInfo() {
-    const {
-      userName,
-      createdAt,
-      email,
-      phoneNumber,
-      drivingCount,
-      sharingCount,
-    } = this.state.user;
-    console.log(
-      userName,
-      createdAt,
-      email,
-      phoneNumber,
-      drivingCount,
-      sharingCount
-    );
+    const $name = qs(`.${styles["user-name"]}`) as HTMLDivElement;
+    $name.innerText = this.state.user.userName;
+    const $date = qs(`.${styles["user-date"]}`) as HTMLDivElement;
+    $date.innerText = this.state.user.createdAt;
+    const $email = qs(`.${styles["user-email"]}`) as HTMLDivElement;
+    $email.innerText = this.state.user.email;
+    const $expno = qs(`.${styles["user-expno"]}`) as HTMLDivElement;
+    $expno.innerText = this.state.user.sharingCount;
+    const $shrno = qs(`.${styles["user-shareno"]}`) as HTMLDivElement;
+    $shrno.innerText = this.state.user.createdAt;
   }
 
   generateCard(data: IUserData): string {
