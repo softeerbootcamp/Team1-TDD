083b4a40c81bc1c73866ee04b6e76d518c4cf0ad
jieonkim23 <kwldjs1023@gmail.com>
2023-02-16 19:23:33 +0900

    fix: 필드 네이밍 opponentType으로 변경하고 값도 그에 맞춰 변경함
    제목과 유저의 타입이 상이해서 변경하게 됨

--- back-end/src/main/java/com/tdd/backend/mail/data/UserType.java
+++ back-end/src/main/java/com/tdd/backend/mail/data/UserType.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum UserType {
-	OWNER("공유자", "등록하신 차량 공유가 성사되었습니다."),
-	TESTER("시승자", "차량 시승 신청이 완료되었습니다.");
-	private final String name;
+	OWNER("시승자", "등록하신 차량 공유가 성사되었습니다."),
+	TESTER("공유자", "차량 시승 신청이 완료되었습니다.");
+	private final String opponentType;
 	private final String subject;
 
-	UserType(String name, String subject) {
-		this.name = name;
+	UserType(String opponentType, String subject) {
+		this.opponentType = opponentType;
 		this.subject = subject;
 	}
 }
