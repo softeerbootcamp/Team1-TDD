75708df2a7338ab71c08c8316a57bca9e62c89c0
jieonkim23 <kwldjs1023@gmail.com>
2023-02-16 21:03:04 +0900

    refactor: 필요없는 삭제들 삭제

--- back-end/src/main/java/com/tdd/backend/mail/EmailSendable.java
+++
@@ -1,8 +0,0 @@
-package com.tdd.backend.mail;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface EmailSendable {
-	void send(String[] to, String subject, String message) throws InterruptedException;
-}
