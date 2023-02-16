4c8a8eab43cb9e50b3d39f1b61d1559b48d5dafe
이제영 <82891332+2je0@users.noreply.github.com>
2023-02-16 12:01:52 +0900

    revert: revert ㅜㅜㅜ (#177)

--- front-end/src/components/Navbar/Navbar.ts
+++ front-end/src/components/Navbar/Navbar.ts
@@ -2,6 +2,7 @@ import Component from '@/core/Component';
 import { AuthStore } from '@/store/AuthStore';
 import { openLoginModal } from '@/utils/loginModal';
 import { qs } from '@/utils/querySelector';
+import { userIcon } from './icon';
 import styles from './Navbar.module.scss';
 
 export class Navbar extends Component {
@@ -22,9 +23,18 @@ export class Navbar extends Component {
         <li><a data-link href="/experiencing">경험하기</a></li>
         ${
           isLogin
-            ? `<button class="${styles['logout-button']}">logout</button>`
+            ? `
+            <div class="${styles.dropdown}">
+            ${userIcon}
+              <div class="${styles.options}">
+                <div><a data-link href="/mypage">마이페이지</a></div>
+                <div class="${styles['logout-button']}">Logout</div>
+              </div>
+            </div>
+            `
             : `<button class="${styles['login-button']}">login</button>`
         }
+        
       </ul>
     </nav>
     `;
