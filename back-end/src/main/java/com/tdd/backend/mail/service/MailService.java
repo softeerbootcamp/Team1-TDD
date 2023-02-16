0b13375d8b569566f792a0250e70de63a8bdd3e8
jieonkim23 <kwldjs1023@gmail.com>
2023-02-16 18:51:31 +0900

    feat: 디비에서 가져온 정보들 바탕으로 EmailMessage를 작성하는 메소드 생성

---
+++ back-end/src/main/java/com/tdd/backend/mail/service/MailService.java
@@ -0,0 +1,42 @@
+package com.tdd.backend.mail.service;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.tdd.backend.mail.data.EmailMessage;
+import com.tdd.backend.mail.data.EmailMessage.UserType;
+import com.tdd.backend.mail.data.PostInfo;
+import com.tdd.backend.post.repository.PostRepository;
+import com.tdd.backend.user.data.User;
+import com.tdd.backend.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+	private final JavaMailSender javaMailSender;
+	private final PostRepository postRepository;
+	private final UserRepository userRepository;
+
+
+
+	public EmailMessage generateEmailMessage(PostInfo postInfo, Long anotherId, Long receiverId, UserType userType) {
+		String toEmail = userRepository.findEmailById(receiverId).orElseThrow();
+		User another = userRepository.findById(anotherId).orElseThrow();
+
+		return EmailMessage.builder()
+			.to(toEmail)
+			.subject(userType.getSubject())
+			.carName(postInfo.getCarName())
+			.date(postInfo.getDate())
+			.requirement(postInfo.getRequirement())
+			.email(another.getEmail())
+			.name(another.getUserName())
+			.userType(userType)
+			.phoneNumber(another.getPhoneNumber())
+			.build();
+	}
+}
