dc7bcfd05e7a59187067a1f57865e4c53b6b6da3
이제영 <82891332+2je0@users.noreply.github.com>
2023-02-16 12:36:22 +0900

    revert(#174): experiencing v2 (#178)
    * style: 캘린더 스타일링
    
    * style: 캘린더 버튼 축소
    
    * style: google map size 축소
    
    * style: 옵션 셀렉터 크기 축소
    
    * style: 경험하기 페이지 컴포넌트 배치
    
    차종, 캘린더, 옵션, 지도
    
    * style: 캘린더 재 디자인
    
    * style: 캘린더 hover 디자인 변경
    
    * refactor: 캘린더 로직, onClicktd, markDates 리팩토링
    
    * fix: 달력 움직일 때 중복 로직 제거
    
    * fix: 체크된 날짜 표시 안되던 문제 해결
    
    * fix: 날짜가 없는 날도 클릭되던 문제 해결
    
    * refactor: setState 들어가는 로직(달력 움직일 때 ) 수정
    
    * feat: 캘린더 option store와 연결
    
    * feat: 지도 내위치 찾는 기능 구현
    
    * chore: onChangeDates 메소드 이름 수정
    
    * refactor: onChangeDates 분리
    
    * refactor: image slider, optionform 데이터 분리
    
    * feat: google maps 지도움직이기 기능, 지도 정보 반환하기 기능 스토어와 연결
    
    * feat: 지도 움직이기 기능 zoom 정보 없을 때 default parameter 추가
    
    * feat: 기존 마커 지우는 기능, 새로운 마커를 등록하는 기능 추가(더미 코드 다수)
    
    * feat: 구글 맵 스토어와 연결
    
    앞으로 할 일: 서버에서 데이터 받아와서 marker 찍기
    
    * docs: update readme
    
    * feat: bulletinBoard markup
    
    * feat: 경험하기 페이지에서 게시판목록 배치
    
    * feat: test-driving api의 요청과 응답 데이터 인터페이스 구현
    
    * feat: map 내위치 찾기 기능 구현
    
    * feat: test driving 데이터 핸들러 개발(중)
    
    * Update README.md
    
    * ci: 충돌 해결
    
    * revert: apis 되돌리기
    
    * revert: revert

---
+++ front-end/src/store/OptionStore/DataHandler.ts
@@ -0,0 +1,25 @@
+import { axiosInstance } from '@/apis';
+import { ItestDrivingReq, ItestDrivingRes } from './interface';
+import { IOptionState } from './OptionStore';
+export async function totalDataHandler(state: IOptionState) {
+  const reqBody: ItestDrivingReq = {
+    carName: state.carModel,
+    dateList: state.dates,
+    optionList: state.options,
+  };
+  const response: ItestDrivingRes[] = await axiosInstance.post(
+    '/test-driving/posts',
+    reqBody
+  );
+  const { latHi, latLo, lngHi, lngLo } = state.mapInfo;
+  const filteredPost = response.filter((ele) => {
+    const { latitude, longitude } = ele.location;
+    return (
+      latHi > +latitude &&
+      latLo < +latitude &&
+      lngHi > +longitude &&
+      lngLo < +longitude
+    );
+  });
+  return { ...state, filteredPost };
+}
