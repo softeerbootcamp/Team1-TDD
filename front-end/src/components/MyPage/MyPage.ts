7c33bb5454640a04ba38da6d7d23d8ff0ce0d561
MyuB <mjycoco0@naver.com>
2023-02-16 15:38:01 +0900

    feat: 마이페이지 api 연동 추가 및 마크업 변경에 따른 로직 수정

--- front-end/src/components/MyPage/MyPage.ts
+++ front-end/src/components/MyPage/MyPage.ts
@@ -1,55 +1,194 @@
-import { getUserInfo } from "@/apis/mypage";
-import Component from "@/core/Component";
-import { qs } from "@/utils/querySelector";
-import { literal } from "./template";
-import styles from "./MyPage.module.scss";
+import { getUserInfo } from '@/apis/mypage';
+import Component from '@/core/Component';
+import { qs } from '@/utils/querySelector';
+import { literal } from './template';
+import styles from './MyPage.module.scss';
+import { loadscript } from '@/utils/googleAPI';
+import { carList } from '@/constants/carList';
 
-interface IUserData {
-  carName: string;
-  options: object;
+interface IAppointment {
   date: string;
-  location: string;
+  id: number;
+  status: string;
+}
+
+interface IOptions {
+  category: string;
+  name: string;
+}
+
+interface ILocation {
+  latitude: string;
+  longitude: string;
+}
+
+interface ISharing {
+  appointments: IAppointment[];
+  post: {
+    carName: string;
+    id: number;
+    location: ILocation;
+    options: IOptions[];
+    requirement: string;
+    rideOption: string;
+  };
+}
+
+interface IDriving {
+  date: string;
+  post: {
+    carName: string;
+    id: number;
+    location: ILocation;
+    options: IOptions[];
+    requirement: string;
+    rideOption: string;
+  };
+}
+
+interface IUser {
+  createdAt: string;
+  drivingCount: number;
+  email: string;
+  phoneNumber: string;
+  sharingCount: number;
+  userName: string;
 }
 
 export class MyPage extends Component {
-  setup(): void {
+  setup() {
     getUserInfo()
       .then((res) => {
-        this.state.driving = res.data.driving;
-        this.state.sharing = res.data.sharing;
-        this.state.user = res.data.user;
-        console.log(this.state.user);
-        console.log(res);
-        console.log("hi");
+        this.setState({ res });
       })
       .catch((err) => console.log(err));
+    this.init();
   }
 
   template(): string {
     return literal();
   }
 
+  init() {
+    loadscript(
+      `https://maps.googleapis.com/maps/api/js?key=${process.env.VITE_API_KEY}&callback=initMap`,
+      function doNothing() {}
+    );
+  }
+
   mounted(): void {
-    const $expBoard = qs(`.${styles["exp-board"]}`) as HTMLDivElement;
-    const $shareBoard = qs(`.${styles["share-board"]}`) as HTMLDivElement;
+    const shareCardWrapper = qs(`#${'share-card'}`);
+    const expCardWrapper = qs(`#${'exp-card'}`);
+
+    const drivingList = this.state.res.data.driving as IDriving[];
+    const sharingList = this.state.res.data.sharing as ISharing[];
+    const user = this.state.res.data.user as IUser;
+
+    console.log(this.state.res.data);
+    this.setUserInfo(user);
+    drivingList?.forEach((element: IDriving) => {
+      expCardWrapper!.innerHTML += this.generateDrivingCard(element);
+    });
+    sharingList.forEach((element: ISharing) => {
+      shareCardWrapper!.innerHTML += this.generateSharingCard(element);
+    });
   }
 
-  setUserInfo() {
-    const $name = qs(`.${styles["user-name"]}`) as HTMLDivElement;
-    $name.innerText = this.state.user.userName;
-    const $date = qs(`.${styles["user-date"]}`) as HTMLDivElement;
-    $date.innerText = this.state.user.createdAt;
-    const $email = qs(`.${styles["user-email"]}`) as HTMLDivElement;
-    $email.innerText = this.state.user.email;
-    const $expno = qs(`.${styles["user-expno"]}`) as HTMLDivElement;
-    $expno.innerText = this.state.user.sharingCount;
-    const $shrno = qs(`.${styles["user-shareno"]}`) as HTMLDivElement;
-    $shrno.innerText = this.state.user.createdAt;
+  async reverseGeocode(latlng: object) {
+    const LatLng = latlng as google.maps.LatLng;
+    const geocoder = new google.maps.Geocoder();
+    let content = '';
+    geocoder.geocode({ location: LatLng }, (response) => {
+      content = response[0].formatted_address;
+    });
+    return content;
   }
 
-  generateCard(data: IUserData): string {
+  setUserInfo(user: IUser) {
+    const userInfo = qs(`.${styles['user-info']}`) as HTMLDivElement;
+    const $name = qs(`.${styles['user-name']}`, userInfo) as HTMLDivElement;
+    $name.innerText = user.userName;
+    const $date = qs(`.${styles['user-date']}`, userInfo) as HTMLDivElement;
+    $date.innerText = user.createdAt;
+    const $email = qs(`.${styles['user-email']}`, userInfo) as HTMLDivElement;
+    $email.innerText = user.email;
+    const $expno = qs(`.${styles['user-exp']}`, userInfo) as HTMLDivElement;
+    $expno.innerText = user.drivingCount as unknown as string;
+    const $shrno = qs(`.${styles['user-share']}`, userInfo) as HTMLDivElement;
+    $shrno.innerText = user.sharingCount as unknown as string;
+  }
+
+  findCarImage(carName: string) {
+    const matched = carList.filter((car) => car.fileName.includes(carName));
+    return matched.length === 0 ? null : matched[0].fileName;
+  }
+
+  generateDrivingCard(data: IDriving): string {
+    const carName = data.post.carName.toLowerCase();
+    const carImage = this.findCarImage(carName);
+    const options = data.post.options;
+    const date = data.date;
+    const lat = +data.post.location.latitude;
+    const lng = +data.post.location.longitude;
+    const loc = {
+      lat: lat,
+      lng: lng,
+    };
+    debugger;
+    const location = this.reverseGeocode(loc);
     return `
-    
+    <div class="${styles['card-wrapper']}">
+      <div class=${styles['image-wrapper']}>
+        <div class="${styles['helper']}"></div>
+        <img class="${styles['image']}" src="${
+      process.env.VITE_IMAGE_URL
+    }/${carImage}" />
+      </div>
+      <div class="${styles['text-wrapper']}">
+        <div class="${styles['helper']}"></div>
+        <div class="${styles['car-name']}">${carName}</div>
+        <div class="${styles['options']}">${options.map(
+      (ele) => ele.name
+    )}</div>
+        <div class="${styles['date']}">${date}</div>
+        <div class="${styles['location']}">${location}</div>
+      </div>
+    </div>
+    `;
+  }
+
+  generateSharingCard(data: ISharing): string {
+    const carName = data.post.carName.toLowerCase();
+    const carImage = this.findCarImage(carName);
+    const options = data.post.options;
+    const lat = +data.post.location.latitude;
+    const lng = +data.post.location.longitude;
+    const loc = {
+      lat: lat,
+      lng: lng,
+    };
+    const location = this.reverseGeocode(loc);
+    // const appointment = data.appointments;
+    // <div class="${styles['date']}">${appointment[0].date}</div>
+    // 이거는 고민 좀 해봐야할 듯
+    return `
+    <div class=${styles['card-wrapper']}>
+      <div class=${styles['image-wrapper']}>
+        <div class=${styles['helper']}></div>
+        <img class="${styles['image']}" src="${
+      process.env.VITE_IMAGE_URL
+    }/${carImage}" />
+      </div>
+      <div class="${styles['text-wrapper']}">
+        <div class="${styles['helper']}"></div>
+        <div class="${styles['car-name']}">${carName}</div>
+        <div class="${styles['options']}">${options.map(
+      (ele) => ele.name
+    )}</div>
+        
+        <div class="${styles['location']}">${location}</div>
+      </div>
+    </div>
     `;
   }
 }
