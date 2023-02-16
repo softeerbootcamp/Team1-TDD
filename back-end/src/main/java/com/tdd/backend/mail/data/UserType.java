6e1d366bb91877c2705aaae4e646a92e80931ea4
jieonkim23 <kwldjs1023@gmail.com>
2023-02-16 19:06:05 +0900

    refactor: 필드 final 로 변경

--- back-end/src/main/java/com/tdd/backend/mail/data/UserType.java
+++ back-end/src/main/java/com/tdd/backend/mail/data/UserType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 public enum UserType {
 	OWNER("공유자", "등록하신 차량 공유가 성사되었습니다."),
 	TESTER("시승자", "차량 시승 신청이 완료되었습니다.");
-	private String name;
-	private String subject;
+	private final String name;
+	private final String subject;
 
 	UserType(String name, String subject) {
 		this.name = name;
