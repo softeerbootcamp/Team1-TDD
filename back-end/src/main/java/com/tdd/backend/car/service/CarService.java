447c2e556a6e3fd8f7e5e942cf0f1aafdc9294df
River-Mt <rivermt1998@gmail.com>
2023-02-16 17:18:15 +0900

    feat: 모든 차량의 정보를 반환하는 메서드 구현

--- back-end/src/main/java/com/tdd/backend/car/service/CarService.java
+++ back-end/src/main/java/com/tdd/backend/car/service/CarService.java
@@ -1,8 +1,13 @@
 package com.tdd.backend.car.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.tdd.backend.car.data.CarDto;
 import com.tdd.backend.car.exception.CarNotFoundException;
+import com.tdd.backend.car.model.Car;
 import com.tdd.backend.car.repository.CarRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +19,13 @@ public class CarService {
 
 	public Long findCarId(String carName) {
 		return carRepository.findIdByCarName(carName).orElseThrow(CarNotFoundException::new);
+	}
+
+	public List<CarDto> findAllCar() {
+		List<Car> carList = carRepository.findAllCar();
+
+		return carList.stream()
+			.map(Car::teCarDto)
+			.collect(Collectors.toList());
 	}
 }
