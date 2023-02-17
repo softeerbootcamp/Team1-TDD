498101a3c6b56fc7a13735d4a0414d37bc575f0d
jieonkim23 <kwldjs1023@gmail.com>
2023-02-17 10:50:30 +0900

    refactor: url을 줄이고 이미지 html을 상수로 분리, p태그 적용

--- back-end/src/main/java/com/tdd/backend/mail/data/EmailMessage.java
+++ back-end/src/main/java/com/tdd/backend/mail/data/EmailMessage.java
@@ -15,6 +15,8 @@ public class EmailMessage {
 	private final String requirement;
 	private final String subject;
 	private final UserType userType;
+	private static final String image = "<a target=\"_blank\" href=\"https://letstdd.site\">"
+		+ "<img style=\"max-width:30%; height:auto;\" src =\"https://url.kr/mqafdo\"/></a>";
 
 	@Builder
 	private EmailMessage(String to, String name, String phoneNumber, String email, String date, String carName,
@@ -34,13 +36,14 @@ public class EmailMessage {
 		String userStr = userType.getOpponentType();
 
 		StringBuilder sb = new StringBuilder();
-		sb.append(userStr + " 성함: " + name + "\n");
-		sb.append(userStr + " 번호: " + phoneNumber + "\n");
-		sb.append(userStr + " 메일: " + email + "\n");
-		sb.append("-------------------------\n");
-		sb.append("차종: " + carName + "\n");
-		sb.append("시승 날짜: " + date + "\n");
-		sb.append("세부 사항: " + requirement);
+		sb.append(image);
+		sb.append("<p>" + userStr + " 성함: " + name + "</p>");
+		sb.append("<p>" + userStr + " 번호: " + phoneNumber + "</p>");
+		sb.append("<p>" + userStr + " 메일: " + email + "</p>");
+		sb.append("-------------------------------</p>");
+		sb.append("<p>" + "차종: " + carName + "</p>");
+		sb.append("<p>" + "시승 날짜: " + date + "</p>");
+		sb.append("<p>" + "세부 사항: " + requirement + "</p>");
 		return sb.toString();
 	}
 
