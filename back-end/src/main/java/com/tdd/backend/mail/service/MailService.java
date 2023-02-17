aed0d0f955bcd7ff4e35ff34f61b0a55881d0586
jieonkim23 <kwldjs1023@gmail.com>
2023-02-17 11:04:55 +0900

    feat: 레포지토리 값읽어올때 예외처리

--- back-end/src/main/java/com/tdd/backend/mail/service/MailService.java
+++ back-end/src/main/java/com/tdd/backend/mail/service/MailService.java
@@ -10,8 +10,11 @@ import org.springframework.stereotype.Service;
 import com.tdd.backend.mail.data.EmailMessage;
 import com.tdd.backend.mail.data.PostInfo;
 import com.tdd.backend.mail.data.UserType;
+import com.tdd.backend.mail.exception.EmailNotFoundException;
+import com.tdd.backend.mypage.exception.PostNotFoundException;
 import com.tdd.backend.post.repository.PostRepository;
 import com.tdd.backend.user.data.User;
+import com.tdd.backend.user.exception.UserNotFoundException;
 import com.tdd.backend.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -26,7 +29,8 @@ public class MailService {
 	private final UserRepository userRepository;
 
 	public void send(Long appointmentId, Long testerId) {
-		PostInfo postInfo = postRepository.findPostInfoByAppointmentId(appointmentId).orElseThrow();
+		PostInfo postInfo = postRepository.findPostInfoByAppointmentId(appointmentId)
+			.orElseThrow(PostNotFoundException::new);
 		EmailMessage ownerEmailMessage = generateEmailMessage(postInfo, testerId, postInfo.getUserId(),
 			UserType.OWNER);
 		EmailMessage testerEmailMessage = generateEmailMessage(postInfo, postInfo.getUserId(), testerId,
@@ -50,8 +54,10 @@ public class MailService {
 	}
 
 	public EmailMessage generateEmailMessage(PostInfo postInfo, Long anotherId, Long receiverId, UserType userType) {
-		String toEmail = userRepository.findEmailById(receiverId).orElseThrow();
-		User another = userRepository.findById(anotherId).orElseThrow();
+		String toEmail = userRepository.findEmailById(receiverId)
+			.orElseThrow(EmailNotFoundException::new);
+		User another = userRepository.findById(anotherId)
+			.orElseThrow(UserNotFoundException::new);
 
 		return EmailMessage.builder()
 			.to(toEmail)
