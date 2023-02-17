7f4aa83a14e940720feaf7830e6dd7eee5a0987d
jieonkim23 <kwldjs1023@gmail.com>
2023-02-17 10:57:51 +0900

    fix: 제목 앞에 TDD 추가

--- back-end/src/main/java/com/tdd/backend/mail/data/UserType.java
+++ back-end/src/main/java/com/tdd/backend/mail/data/UserType.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum UserType {
-	OWNER("시승자", "등록하신 차량 공유가 성사되었습니다."),
-	TESTER("공유자", "차량 시승 신청이 완료되었습니다.");
+	OWNER("시승자", "TDD: 등록하신 차량 공유가 성사되었습니다."),
+	TESTER("공유자", "TDD: 차량 시승 신청이 완료되었습니다.");
+
 	private final String opponentType;
 	private final String subject;
 
