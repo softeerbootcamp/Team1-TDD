8337d553c41e7f4b93e3b67f0b43b08667e95fab
River-Mt <rivermt1998@gmail.com>
2023-02-20 01:01:43 +0900

    feat: MyCarService에 내 차 저장하기 메서드 구현

---
+++ back-end/src/main/java/com/tdd/backend/mypage/service/MyCarService.java
@@ -0,0 +1,29 @@
+package com.tdd.backend.mypage.service;
+
+import org.springframework.stereotype.Service;
+
+import com.tdd.backend.car.exception.CarNotFoundException;
+import com.tdd.backend.car.repository.CarRepository;
+import com.tdd.backend.mypage.MyCarRepository;
+import com.tdd.backend.mypage.data.MyCarCreate;
+import com.tdd.backend.mypage.data.MyCarDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class MyCarService {
+	private final MyCarRepository myCarRepository;
+	private final CarRepository carRepository;
+
+	public void save(Long userId, MyCarCreate myCarCreate) {
+		String carName = myCarCreate.getCarName();
+		MyCarDto myCarDto = MyCarDto.builder()
+			.userId(userId)
+			.carId(carRepository.findIdByCarName(carName).orElseThrow(CarNotFoundException::new))
+			.options(myCarCreate.getOptionDtoList())
+			.build();
+
+		myCarRepository.save(myCarDto.toEntity());
+	}
+}
