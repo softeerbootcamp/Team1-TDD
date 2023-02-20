d32459634d90ad5fc5e35c6e0eec4c55139a6587
MyuB <mjycoco0@naver.com>
2023-02-20 12:17:36 +0900

    feat: store에서 사용할 dataHandler 추가

---
+++ front-end/src/store/RegisterStore/DataHandler.ts
@@ -0,0 +1,14 @@
+import { axiosInstance } from '@/apis';
+import { IRegisterCarReq } from './interface';
+import { IRegisterState } from './RegisterStore';
+export async function dataHandler(state: IRegisterState) {
+  const reqBody: IRegisterCarReq = {
+    carName: state.carModel,
+    optionList: state.options.map((ele) => ele.name),
+  };
+  //api 받아오면 수정
+  const response = await axiosInstance.post('/test-driving/posts', reqBody);
+  console.log(response.data);
+  //return 할 때 data 배열로 넘겨줘야 함. option store의 datahander 참고
+  return { ...state };
+}
