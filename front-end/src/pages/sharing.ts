9407af7c862b596299f3cd65cb245b35593054bf
2je0 <bulljy@naver.com>
2023-02-20 18:12:09 +0900

    feat: route guard 구현

--- front-end/src/pages/sharing.ts
+++ front-end/src/pages/sharing.ts
@@ -1,14 +1,25 @@
+import { routeGaurd } from '@/apis/login';
 import { SharingForm } from '@/components/SharingForm/SharingForm';
 import Component from '@/core/Component';
 import { qs } from '@/utils/querySelector';
 
 export class Sharing extends Component {
-  setup(): void {}
+  setup(): void {
+    this.state.isLogin = false;
+    routeGaurd(
+      () => {
+        this.setState({ isLogin: true });
+      },
+      () => {
+        location.replace('/');
+      }
+    );
+  }
   template(): string {
-    return `<div id="sharing-form"></div>`;
+    return this.state.isLogin ? '<div id="sharing-form"></div>' : '';
   }
   mounted(): void {
     const $sharingForm = qs('#sharing-form', this.target);
-    new SharingForm($sharingForm as HTMLDivElement);
+    if ($sharingForm) new SharingForm($sharingForm as HTMLDivElement);
   }
 }
