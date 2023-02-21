eb86d680713d91fba71e4dc815e50a34a3b05c2f
MyuB <mjycoco0@naver.com>
2023-02-21 18:18:09 +0900

    feat: patch api 개발

--- front-end/src/apis/detailPage.ts
+++ front-end/src/apis/detailPage.ts
@@ -4,6 +4,14 @@ export const getPosts = async (postId: number) => {
   return axiosInstance.get(`/test-driving/${postId}`);
 };
 
-export const patchAppoinment = async (appoinmentId: string) => {
-  return axiosInstance.patch(`/appointments/${appoinmentId}`);
+export const patchAppoinment = async (appoinmentId: number) => {
+  const accessToken = localStorage.getItem('accessToken');
+
+  return axiosInstance.patch(
+    `/appointments/${appoinmentId}`,
+    {},
+    {
+      headers: { Authorization: accessToken },
+    }
+  );
 };
