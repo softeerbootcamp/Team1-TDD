81e9b1d62461e5ad8bbc8a5bebc70ffc26b7c438
2je0 <bulljy@naver.com>
2023-02-21 17:14:55 +0900

    chore: 주석 제거

--- front-end/src/utils/autoCompletor.ts
+++ front-end/src/utils/autoCompletor.ts
@@ -1,19 +1,15 @@
 export function initAutocomplete(map: google.maps.Map) {
-  // Create the search box and link it to the UI element.
   const input = document.getElementById('pac-input') as HTMLInputElement;
   const searchBox = new google.maps.places.SearchBox(input);
 
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', () => {
     searchBox.setBounds(map.getBounds() as google.maps.LatLngBounds);
   });
 
   let markers: google.maps.Marker[] = [];
 
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
   searchBox.addListener('places_changed', () => {
     const places = searchBox.getPlaces();
 
@@ -21,13 +17,11 @@ export function initAutocomplete(map: google.maps.Map) {
       return;
     }
 
-    // Clear out the old markers.
     markers.forEach((marker) => {
       marker.setMap(null);
     });
     markers = [];
 
-    // For each place, get the icon, name and location.
     const bounds = new google.maps.LatLngBounds();
 
     places.forEach((place) => {
@@ -38,13 +32,12 @@ export function initAutocomplete(map: google.maps.Map) {
 
       const icon = {
         url: place.icon as string,
-        size: new google.maps.Size(71, 71),
+        size: new google.maps.Size(30, 30),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25),
       };
 
-      // Create a marker for each place.
       markers.push(
         new google.maps.Marker({
           map,
@@ -55,7 +48,6 @@ export function initAutocomplete(map: google.maps.Map) {
       );
 
       if (place.geometry.viewport) {
-        // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
       } else {
         bounds.extend(place.geometry.location);
