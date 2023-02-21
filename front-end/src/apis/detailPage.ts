13a1d52765ca7086bccf1c011cc55fa1f02c5e8a
MyuB <mjycoco0@naver.com>
2023-02-21 16:50:14 +0900

    feat: patch api 연동

--- front-end/src/apis/detailPage.ts
+++ front-end/src/apis/detailPage.ts
@@ -3,3 +3,7 @@ import { axiosInstance } from '.';
 export const getPosts = async (postId: number) => {
   return axiosInstance.get(`/test-driving/${postId}`);
 };
+
+export const patchAppoinment = async (appoinmentId: string) => {
+  return axiosInstance.patch(`/appointments/${appoinmentId}`);
+};
